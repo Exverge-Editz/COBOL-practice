b2d32768d092f3ec0f44ff07f549a3d960e1ed7d
callum fromnt <callumfromant@gmail.com>
2025-10-05 21:33:04 +0100

    my first COBOL programme
    this programme displays hello world to the screen

---
+++ hello world.cob
@@ -0,0 +1,6 @@
+000100 identification division.
+000200 program-id. hello-world.
+000300 author. callum fromant.
+000300 procedure division.
+000400     display "Hello, World!".
+000500     stop run.
