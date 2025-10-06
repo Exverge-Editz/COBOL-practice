5257e3334340efa201dbfc53774663e0067994d2
callum fromnt <callumfromant@gmail.com>
2025-10-06 13:27:32 +0100

    Update hello world.cob

--- hello world.cob
+++ hello world.cob
@@ -1,6 +1,7 @@
 000100 identification division.
 000200 program-id. hello-world.
 000300 author. callum fromant.
-000300 procedure division.
-000400     display "Hello, World!".
-000500     stop run.
+000400 procedure division.
+000500     display "Hello, World!".
+000600     stop run.
+
